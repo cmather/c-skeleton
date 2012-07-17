788635870e795c11e892536d24feb1a264c9b43f
Chris Mather <mather.chris@gmail.com>
2012-07-16 19:30:21 -0700

    Standard makefile.

---
+++ src/hello_world.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "hello_world.h"
+#include "dbg.h"
+
+void say_hello()
+{
+  printf("Hello World");
+}
---
+++ src/hello_world.h
@@ -0,0 +1,6 @@
+#ifndef _hello_world_
+#define _hello_world_
+
+void say_hello();
+
+#endif
